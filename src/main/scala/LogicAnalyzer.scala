6e012a371c7c2b2561852db8b29ac81f90c2b02d
ducky <richard.lin@berkeley.edu>
2016-11-03 17:05:37 -0700

    Core state machine and logic, no sampling code yet

--- src/main/scala/LogicAnalyzer.scala
+++ src/main/scala/LogicAnalyzer.scala
@@ -5,7 +5,7 @@ package debuggers
 import chisel3._
 import chisel3.util._
 
-/** Built-in snooping logic analyzer.
+/** Snooping logic analyzer block.
   *
   * @param dataWidth bit width of the data (signal input to sample)
   * @param memWidth bit width of memory (for read out), must be an integer multiple of dataWidth
@@ -16,8 +16,13 @@ class LogicAnalyzer(dataWidth: Int, memWidth: Int, samples: Int) extends Module 
   assert(memWidth % dataWidth == 0)
   assert(samples % (memWidth / dataWidth) == 0)
 
+  //
+  // Common constants
+  //
+
   val samplesWidth = log2Up(samples + 1)
-  val reqAddrWidth = log2Up(samples / (memWidth / dataWidth))
+  val memDepth = samples / (memWidth / dataWidth)
+  val reqAddrWidth = log2Up(memDepth)
 
   // TODO: DRYify
   val sIdle :: sArmed :: sRunning :: Nil = Enum(UInt(), 3)
@@ -26,6 +31,10 @@ class LogicAnalyzer(dataWidth: Int, memWidth: Int, samples: Int) extends Module 
   // TODO: DRYify
   val trigNone, trigHigh :: trigLow :: trigRising :: trigFalling = Enum(UInt(), 5)
   val trigWidth = log2Up(5)
+
+  //
+  // IO Definitions
+  //
 
   /** Signal input being sampled with optional flow control signals.
     */
@@ -70,7 +79,7 @@ class LogicAnalyzer(dataWidth: Int, memWidth: Int, samples: Int) extends Module 
   class LogicAnalyzerControl extends Bundle {
     /** Logic analyzer configuration: ignore the signal valid and sample every clock cycle.
       */
-    val validBypass = Input(Bool())
+    val validBypass = Bool()
     /** Logic analyzer configuration: trigger mode.
       * trigNone: start sampling immediately, on the next valid cycle.
       * trigHigh: start sampling on the first valid cycle where trigger is high.
@@ -80,20 +89,20 @@ class LogicAnalyzer(dataWidth: Int, memWidth: Int, samples: Int) extends Module 
       * trigFalling: start sampling on the first valid cycle where trigger is low, following a
       * valid cycle where trigger was high.
       */
-    val triggerMode = Input(UInt(width=trigWidth))
+    val triggerMode = UInt(width=trigWidth)
     /** Logic analyzer configuration: number of samples to take.
       * Zero means to run in continuous mode, wrapping around the memory write address and
       * overwriting previous samples until stopped through the control abort signal.
       */
-    val numSamples = Input(UInt(width=samplesWidth))
+    val numSamples = UInt(width=samplesWidth)
     /** Arm the logic analyzer, latching in the logic analyzer configuration.
       * Only valid in the idle state, transitions to the armed state.
       */
-    val arm = Input(Bool())
+    val arm = Bool()
     /** Transitions back to the idle state, aborting any capture in progress.
       * Partial samples are kept and status numSampled will be valid.
       */
-    val abort = Input(Bool())
+    val abort = Bool()
 
     override def cloneType: this.type = (new LogicAnalyzerControl).asInstanceOf[this.type]
   }
@@ -105,10 +114,11 @@ class LogicAnalyzer(dataWidth: Int, memWidth: Int, samples: Int) extends Module 
     /** Current state the logic analyzer is in.
       */
     val state = Output(UInt(width=stateWidth))
-    /** Number of valid samples in the buffer, between 0 and `samples`.
+    /** Number of valid samples in the buffer, between 0 and `samples`, inclusive.
       * 0 means no samples are valid.
       *
-      * In continuous mode, this will roll over from `samples` to 1.
+      * In continuous mode, this will roll over from `samples` to 1, indicating the next memory
+      * address to be written.
       */
     val numSampled = Output(UInt(log2Up(samples + 1)))
     /** In continuous mode, indicates if the numSampled has ever overflowed.
@@ -122,13 +132,97 @@ class LogicAnalyzer(dataWidth: Int, memWidth: Int, samples: Int) extends Module 
   }
 
   class LogicAnalyzerIO extends Bundle {
-    val signal = new LogicAnalyzerStatus
+    val signal = new LogicAnalyzerSignal
     val memory = new LogicAnalyzerMemory
-    val control = Decoupled(new LogicAnalyzerControl)
+    val control = Flipped(Decoupled(new LogicAnalyzerControl))
     val status = new LogicAnalyzerStatus
 
     override def cloneType: this.type = (new LogicAnalyzerIO).asInstanceOf[this.type]
   }
 
-  val io = new LogicAnalyzerIO
+  val io = IO(new LogicAnalyzerIO)
+
+  //
+  // Logic Analyzer Logic
+  //
+
+  val buffer = SeqMem(UInt(width=memWidth), memDepth)
+  val state = Reg(UInt(width=stateWidth), init=sIdle)
+  val nextState = Wire(UInt(width=stateWidth))
+  val nextAddress = Reg(UInt(width=samplesWidth))
+
+  io.status.state := state
+  io.status.numSampled := nextAddress
+
+  // Logic Analyzer Configuration
+  val confValidBypass = Reg(Bool())
+  val confTriggerMode = Reg(UInt(width=trigWidth))
+  val confNumSamples = Reg(UInt(width=samplesWidth))
+
+  //
+  // Sampling Control
+  //
+  val internalValid = confValidBypass || io.signal.valid  // sample when true
+  val lastTrigger = Reg(Bool())  // previous trigger value
+  val lastTriggerValid = Reg(Bool())  // whether the previous trigger value is valid (sampled after arming)
+
+  when (internalValid && state === sArmed) {
+    lastTrigger := io.signal.trigger
+    lastTriggerValid := true.B
+  } .elsewhen (state === sIdle) {
+    lastTriggerValid := false.B
+  }
+
+  val internalTrigger = Wire(Bool())  // high means start sampling this cycle
+  switch (confTriggerMode) {
+  is (trigNone) {
+    internalTrigger := internalValid
+  }
+  is (trigHigh) {
+    internalTrigger := internalValid && io.signal.trigger
+  }
+  is (trigLow) {
+    internalTrigger := internalValid && !io.signal.trigger
+  }
+  is (trigRising) {
+    internalTrigger := internalValid && io.signal.trigger && lastTriggerValid && !lastTrigger
+  }
+  is (trigFalling) {
+    internalTrigger := internalValid && !io.signal.trigger && lastTriggerValid && lastTrigger
+  }
+  }
+
+  //
+  // Memory Interface & Control
+  //
+  val nextNextAddress = Wire(UInt(width=samplesWidth))
+  when (state === sRunning && internalValid) {
+    nextNextAddress := nextAddress + 1.U
+  } .otherwise {
+    nextNextAddress := nextAddress
+  }
+  nextAddress := nextNextAddress
+
+
+  //
+  // State Machine
+  //
+  io.control.ready := true.B
+  when (io.control.bits.abort && io.control.valid) {
+    nextState := sIdle
+  } .elsewhen (state === sIdle && io.control.bits.arm && io.control.valid) {
+    nextState := sArmed
+    confValidBypass := io.control.bits.validBypass
+    confTriggerMode := io.control.bits.triggerMode
+    confNumSamples := io.control.bits.numSamples
+    nextAddress := 0.U
+  } .elsewhen (state === sArmed && internalTrigger) {
+    nextState := sRunning
+  } .elsewhen (state === sRunning && nextNextAddress === confNumSamples && confNumSamples != 0.U) {
+    nextState := sIdle
+  } .otherwise {
+    nextState := state
+  }
+
+  state := nextState
 }
