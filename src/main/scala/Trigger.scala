1b8b0ea4e331a6cf8cfebf759041c2bef3010671
ducky <richard.lin@berkeley.edu>
2016-11-08 13:38:39 -0800

    Trigger beginnings

---
+++ src/main/scala/Trigger.scala
@@ -0,0 +1,13 @@
+// See LICENSE for license details.
+
+package debuggers
+
+import chisel3._
+import chisel3.util._
+
+/** Common trigger block and utilities.
+  */
+
+class Trigger extends Module {
+
+}
