dbac693c3c7e126ef4a3fef077faaa32a3390d6f
ducky <richard.lin@berkeley.edu>
2017-03-23 13:39:01 -0700

    Update interface spec to new tester style

--- src/test/scala/PatternGeneratorInterfaceSpec.scala
+++ src/test/scala/PatternGeneratorInterfaceSpec.scala
@@ -20,17 +20,17 @@ trait PatternGeneratorImplicitTester extends ImplicitPokeTester {
       numSamples: Int, continuous: Boolean)(implicit t: InnerTester) {
     require(numSamples > 0)
 
-    io.status.state ?== PatternGeneratorState.Idle.id
-    io.control.bits.readyBypass <<= readyBypass
-    io.control.bits.triggerMode <<= triggerMode.id
-    io.control.bits.lastSample <<= numSamples - 1
-    io.control.bits.continuous <<= continuous
-    io.control.bits.arm <<= 1
-    io.control.bits.abort <<= 0
-    io.control.valid <<= 1
+    check(io.status.state, PatternGeneratorState.Idle.id)
+    poke(io.control.bits.readyBypass, readyBypass)
+    poke(io.control.bits.triggerMode, triggerMode.id)
+    poke(io.control.bits.lastSample, numSamples - 1)
+    poke(io.control.bits.continuous, continuous)
+    poke(io.control.bits.arm, 1)
+    poke(io.control.bits.abort, 0)
+    poke(io.control.valid, 1)
     step()
-    io.control.valid <<= 0
-    io.status.state ?== PatternGeneratorState.Armed.id
+    poke(io.control.valid, 0)
+    check(io.status.state, PatternGeneratorState.Armed.id)
   }
 
   /** Writes the buffer using the memory interface, returns the number of samples written.
@@ -40,17 +40,17 @@ trait PatternGeneratorImplicitTester extends ImplicitPokeTester {
     var numSamples: Int = 0
 
     for ((line, i) <- contents.zipWithIndex) {
-      io.status.state ?== PatternGeneratorState.Idle.id
-      io.memory.ready ?== 1
-      io.memory.bits.writeAddr <<= i
-      io.memory.valid <<= 1
+      check(io.status.state, PatternGeneratorState.Idle.id)
+      check(io.memory.ready, 1)
+      poke(io.memory.bits.writeAddr, i)
+      poke(io.memory.valid, 1)
       for ((data, j) <- line.zipWithIndex) {
-        io.memory.bits.writeData(j) <<= data
+        poke(io.memory.bits.writeData(j), data)
         numSamples += 1
       }
       step()
     }
-    io.memory.valid <<= 0
+    poke(io.memory.valid, 0)
 
     numSamples
   }
@@ -58,42 +58,42 @@ trait PatternGeneratorImplicitTester extends ImplicitPokeTester {
   def generatorStep(io: PatternGenerator#PatternGeneratorIO, expectedState: PatternGeneratorStateType, expectedSignal: Option[Int],
       expectedSampled: SampleState, ready: Boolean = true, trigger: TriggerState = TInvalid,
       expectedOverflow: Boolean = false)(implicit t: InnerTester) {
-    io.status.state ?== expectedState.id
+    check(io.status.state, expectedState.id)
 
     trigger match {
-      case THigh | TLow => io.trigger.valid <<= 1
-      case TInvalid | TInvHigh | TInvLow => io.trigger.valid <<= 0
+      case THigh | TLow => poke(io.trigger.valid, 1)
+      case TInvalid | TInvHigh | TInvLow => poke(io.trigger.valid, 0)
     }
     trigger match {
-      case THigh | TInvalid | TInvHigh => io.trigger.bits <<= 1
-      case TLow | TInvLow => io.trigger.bits <<= 0
+      case THigh | TInvalid | TInvHigh => poke(io.trigger.bits, 1)
+      case TLow | TInvLow => poke(io.trigger.bits, 0)
     }
 
     expectedSignal match {
       case Some(expectedSignal) => {
-        io.signal.valid ?== true
-        io.signal.bits ?== expectedSignal
+        check(io.signal.valid, true)
+        check(io.signal.bits, expectedSignal)
       }
-      case None => io.signal.valid ?== false
+      case None => check(io.signal.valid, false)
     }
     expectedSampled match {
       case Started(sample) => {
-        io.status.numSampled ?== sample
-        io.status.started ?== true
-        io.status.overflow ?== false
+        check(io.status.numSampled, sample)
+        check(io.status.started, true)
+        check(io.status.overflow, false)
       }
       case Overflow(sample) => {
-        io.status.numSampled ?== sample
-        io.status.started ?== true
-        io.status.overflow ?== true
+        check(io.status.numSampled, sample)
+        check(io.status.started, true)
+        check(io.status.overflow, true)
       }
       case NotStarted => {
-        io.status.started ?== false
-        io.status.overflow ?== false
+        check(io.status.started, false)
+        check(io.status.overflow, false)
       }
     }
 
-    io.signal.ready <<= ready
+    poke(io.signal.ready, ready)
 
     step()
   }
@@ -101,7 +101,7 @@ trait PatternGeneratorImplicitTester extends ImplicitPokeTester {
 
 class PatternGeneratorInterfaceSpec extends FlatSpec with PatternGeneratorImplicitTester {
   "PatternGenerator StreaminigMemoryQueue" should "work" in {
-    run(new Module {
+    test(new Module {
       val pg = Module(new PatternGenerator(8, 1, 4, false))
       val saq = Module(new StreamingAddressQueue(pg.io.memory.bits.writeData.cloneType, pg.memDepth))
 
@@ -129,34 +129,33 @@ class PatternGeneratorInterfaceSpec extends FlatSpec with PatternGeneratorImplic
 
       io.saqAddr <> saq.io.addr
     }) {implicit t => c =>
-      c.io.pgIo.control.valid <<= false
-      c.io.saqReset <<= false
+      poke(c.io.pgIo.control.valid, false)
+      poke(c.io.saqReset, false)
 
       // Stream data through
-      c.io.saqInput.valid <<= true
-
-      c.io.saqInput.bits(0) <<= 1
-      c.io.saqAddr ?== 0
-      c.io.saqInput.ready ?== true
+      poke(c.io.saqInput.valid, true)
+      poke(c.io.saqInput.bits(0), 1)
+      check(c.io.saqAddr, 0)
+      check(c.io.saqInput.ready, true)
       step()
 
-      c.io.saqInput.bits(0) <<= 2
-      c.io.saqAddr ?== 1
-      c.io.saqInput.ready ?== true
+      poke(c.io.saqInput.bits(0), 2)
+      check(c.io.saqAddr, 1)
+      check(c.io.saqInput.ready, true)
       step()
 
-      c.io.saqInput.bits(0) <<= 3
-      c.io.saqAddr ?== 2
-      c.io.saqInput.ready ?== true
+      poke(c.io.saqInput.bits(0), 3)
+      check(c.io.saqAddr, 2)
+      check(c.io.saqInput.ready, true)
       step()
 
-      c.io.saqInput.bits(0) <<= 4
-      c.io.saqAddr ?== 3
-      c.io.saqInput.ready ?== true
+      poke(c.io.saqInput.bits(0), 4)
+      check(c.io.saqAddr, 3)
+      check(c.io.saqInput.ready, true)
       step()
 
-      c.io.saqAddr ?== 0  // wraparound behavior
-      c.io.saqInput.valid <<= false
+      check(c.io.saqAddr, 0)  // wraparound behavior
+      poke(c.io.saqInput.valid, false)
 
       // Test read-out
       arm(c.io.pgIo, true, TriggerMode.None, 4, false)
